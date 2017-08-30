35e8230e384da68b2a2c9177f2b62798788666b0
Jered Dowden <jereddowden@stackframe.com>
2017-08-29 21:08:16 -0400

    Initial commit with working extraction for macOS

--- renderer.js
+++ renderer.js
@@ -1,3 +1,27 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const fs = require("fs");
+const SevenZipPath = require('7zip-bin');
+const Zip = require('node-7z');
+const myTask = new Zip();
+
+function beginExtraction(name,dest) {
+    myTask.extractFull(name, dest)
+        .progress(function (files) {
+          console.log('Some files are extracted: %s', files);
+        })
+        .then(function () {
+          console.log('Extracting done!');
+        })
+        .catch(function (err) {
+          console.error(err);
+        });
+}
+
+function handleFileSelect(fileList) {
+    fs.readdir(fileList, (err, files) => {
+        for (var i = 0, f; f = files[i]; i++) {
+            if (f.type === 'application/x-7z-compressed' || f.type === 'application/zip') {
+                beginExtraction(f, '/');
+            }
+        }
+    });
+}
